691a23a3e427acd6f38a23134e8c19d5c387fe49
Mokshagna-1760 <97424848+Mokshagna-1760@users.noreply.github.com>
2023-12-26 19:56:54 +0530

    Update program-4.py

--- program-4.py
+++ program-4.py
@@ -1,20 +1,28 @@
 import numpy as np
 import pandas as pd
 
+# Function to calculate weather data based on input parameters
 def calculate_weather(wind, pressure, humidity):
     x = np.linspace(-10, 10, 100)
     y = wind * x**2 + pressure * x + humidity
     return x, y
 
+# Read data from CSV file into a pandas DataFrame
 data = pd.read_csv('program-4.csv')
 
+# Iterate through each row in the DataFrame
 for index, row in data.iterrows():
+    # Extract values from the current row
     wind = row['a']
     pressure = row['b']
     humidity = row['c']
     
+    # Call the calculate_weather function
     x_values, y_values = calculate_weather(wind, pressure, humidity)
     
+    # Display generated weather data for the current set
     print(f"Generated Weather Data for Set {index + 1}:")
+    
+    # Iterate through x_values and y_values
     for i in range(len(x_values)):
-        print(f"x={x_values[i]}, y={y_values[i]}")
+        print(f"  x={x_values[i]}, y={y_values[i]}")  # Proper indentation for readability
